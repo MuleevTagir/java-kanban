ce2468a0dd1596298c7160c2fdb43b959b340ee9
MuleevTagir <granik2007@yandex.ru>
2024-07-13 12:23:20 +0300

    Исправление замечаний по codestyle.

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -8,23 +8,42 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getTaskList();
+
     List<Subtask> getSubtaskList();
+
     List<Epic> getEpicList();
+
     Task getTaskById(Integer id);
+
     Subtask getSubtaskById(Integer id);
+
     Epic getEpicById(Integer id);
+
     Task addTask(Task task);
+
     Subtask addSubtask(Epic epic, Subtask subtask);
+
     Epic addEpic(Epic epic);
+
     void updateTask(Task task);
+
     void updateSubtask(Subtask subtask);
+
     void updateEpic(Epic epic);
+
     void removeTaskById(Integer id);
+
     void removeSubtaskById(Integer id);
+
     void removeEpicById(Integer id);
+
     void removeTaskAll();
+
     void removeSubtaskAll();
+
     void removeEpicAll();
+
     List<Subtask> getSubtaskListByEpic(Epic epic);
+
     List<Task> getHistory();
 }
